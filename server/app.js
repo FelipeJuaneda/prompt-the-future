c758d86acf14ff70f43d20bd3d52201b41a67f0f
Felipe Juaneda <felipejua9@gmail.com>
2024-05-10 13:34:12 -0300

    solucionando cors

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 //Middleware for handling CORS POLICY
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://prompt-the-future-trok.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
     credentials: true,
