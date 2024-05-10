5c6fa64b907ce169e17f3939b0e8628f56ff298f
Felipe Juaneda <felipejua9@gmail.com>
2024-05-10 15:41:19 -0300

    prueba 4

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: true,
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
