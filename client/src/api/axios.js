416783b1bb1543ab4f8ab1b24bed7fee02f6f0ab
Felipe Juaneda <felipejua9@gmail.com>
2024-05-11 21:26:48 -0300

    Cambiando nombre nuevamente en variables de entorno

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
 export default instance;
